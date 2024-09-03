a4db6a33d8214082af2d4143236156174e19aebc
sanketares <sanketares@gmail.com>
2024-09-02 21:52:31 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,20 @@ provider "aws" {
 
 resource "aws_instance" "my_instance" {
   ami           = data.aws_ami.redhat.id
-  instance_type = "t2.micro"               # Instance type
+  instance_type = "t2.micro"  
+  subnet_id     = aws_subnet.my_subnet.id
+
+
+  tags = {
+    Name = "MyEC2Instance-new"
+  }
+}
+
+
+
+resource "aws_instance" "my_instance-added" {
+  ami           = data.aws_ami.redhat.id
+  instance_type = "t2.micro"          
   subnet_id     = aws_subnet.my_subnet.id
 
 
