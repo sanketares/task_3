82056baa22914dd30542d8275fd1f026b63cd875
sanketares <sanketares@gmail.com>
2024-09-02 20:26:58 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,83 +2,11 @@ provider "aws" {
   region = "us-west-2"  # Change this to your desired region
 }
 
-# Create a VPC
-resource "aws_vpc" "my_vpc" {
-  cidr_block = "10.0.0.0/16"
-  enable_dns_support = true
-  enable_dns_hostnames = true
-  tags = {
-    Name = "my-vpc"
-  }
-}
 
-# Create a subnet within the VPC
-resource "aws_subnet" "my_subnet" {
-  vpc_id            = aws_vpc.my_vpc.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-west-2a"
-  map_public_ip_on_launch = true
-  tags = {
-    Name = "my-subnet"
-  }
-}
-
-# Create an Internet Gateway and attach it to the VPC
-resource "aws_internet_gateway" "my_igw" {
-  vpc_id = aws_vpc.my_vpc.id
-  tags = {
-    Name = "my-igw"
-  }
-}
-
-# Create a route table and associate it with the subnet
-resource "aws_route_table" "my_route_table" {
-  vpc_id = aws_vpc.my_vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.my_igw.id
-  }
-
-  tags = {
-    Name = "my-route-table"
-  }
-}
-
-resource "aws_route_table_association" "my_route_table_association" {
-  subnet_id      = aws_subnet.my_subnet.id
-  route_table_id = aws_route_table.my_route_table.id
-}
-
-
-data "aws_ami" "redhat" {
-  most_recent      = true
-  owners           = ["amazon"]
-
-  filter {
-    name   = "name"
-    values = ["RHEL-9*HVM-2024*-x86_64*GP3"]
-  }
-
-  filter {
-    name   = "root-device-type"
-    values = ["ebs"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  filter {
-    name   = "architecture"
-    values = ["x86_64"]
-  }
-}
 
 resource "aws_instance" "my_instance" {
   ami           = data.aws_ami.redhat.id
-  instance_type = "t2.micro"               # Instance type
+  instance_type = "t2.microo"               # Instance type
   subnet_id     = aws_subnet.my_subnet.id
 
 
