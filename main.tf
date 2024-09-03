9ebdbc881fcb82ee92010da9a410a6c6ff4037ea
sanketares <sanketares@gmail.com>
2024-09-02 21:53:27 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,6 +15,17 @@ resource "aws_instance" "my_instance-test" {
 }
 
 
+resource "aws_instance" "my_instance-added" {
+  ami           = data.aws_ami.redhat.id
+  instance_type = "t2.micro"          
+  subnet_id     = aws_subnet.my_subnet.id
+
+
+  tags = {
+    Name = "MyEC2Instance-new"
+  }
+}
+
 
 
 
