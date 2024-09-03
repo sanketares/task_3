b7284db4c444aa75b9f3c31c3e7417e7648e83e4
sanketares <sanketares@gmail.com>
2024-09-02 22:56:15 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,11 @@ provider "aws" {
   region = "us-west-2"  # Change this to your desired region
 }
 
+resource "aws_instance" "santesttttt" {
+  instance_type = "t2.micro"
+  ami           = "ami-053b0d53c279acc90"
+
+}
 
 
 
